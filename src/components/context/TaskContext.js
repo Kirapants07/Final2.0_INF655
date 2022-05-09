a862fc604d100c0a9e2e49b83bd3552c13ea580c
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-05-09 16:26:52 -0700

    updated CRUD for firebase

--- src/components/context/TaskContext.js
+++ src/components/context/TaskContext.js
@@ -1,39 +1,72 @@
-import { useState, createContext } from "react";
-import {v4 as uuidv4} from 'uuid';
-import TaskData from "../Tasks/TaskData";
- 
+import { useState, createContext, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  addDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  limit,
+  collection,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../firebase.config";
+
 const TaskContext = createContext();
 
 export const TaskProvider = ({children}) => {
-
-    const [taskList, setTaskList] = useState(TaskData );
-    const [taskEdit, setTaskEdit] = useState({
-        task: {},
+    const [isLoading, setIsLoading] = useState();
+    const [movieList, setMovieList] = useState([]);
+    const [movieEdit, setMovieEdit] = useState({
+        movie: {},
         edit: false,
     });
 
-    const addTask= (newTask) => {
-        newTask.id = uuidv4();
-        setTaskList([newTask, ...taskList]);
+    useEffect(() => {
+        const fetchTask = async () => {
+            const movieListRef = doc(db, "movieList");
+            const query = query(movieListRef, orderBy("title"), limit(15))
+            const querySnapshot = await getDocs(query);
+            const movieList = [];
+            querySnapshot.forEach((doc) => {
+                return movieList.push({
+                    id: doc.id, //this is not the moviedb id, but the firebase id
+                    data: doc.data(),
+                });
+            });
+    
+            setMovieList(movieList);
+            setIsLoading(false);
+        }
+    }, []);
+
+    const addMovie= async (newMovie) => {
+        const docRef = await addDoc(collection(db, "movieList"), newMovie);
+        console.log("Movie added with id: ", docRef.id);
+        setMovieList([movieList]);
     };
 
-    const editTask = (task) => {
-        setTaskEdit({task, edit: true});
+    const editMovie = (id, movie) => {
+        setMovieEdit({id, movie, edit: true});
     };
 
-    const updateTask = (id, updTask) => {
-        setTaskList(taskList.map((task)=> task.id === id ? {...task, ...updTask} : task));
+    const updateMovie = async (id, updMovie) => {
+        const docRef = doc(db, "movieList", id);
+        await updateDoc(docRef, updMovie);
     };
 
-    const deleteTask = (id) => {
-        setTaskList(taskList.filter((task) => task.id !== id));
+    const deleteMovie = async (id) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+            await deleteDoc(doc(db, "movieList", id));
+        }
     };
     
     const favorite = (id) => {
-        setTaskList(taskList.map((movie)=> movie.id === id ? {...movie, checked: !movie.checked} : movie));
+        setMovieList(movieList.map((movie)=> movie.id === id ? {...movie, checked: !movie.checked} : movie));
     };
 
-    return <TaskContext.Provider value={{taskList, taskEdit, deleteTask, checkTask: favorite, addTask, editTask, updateTask}}>{children}</TaskContext.Provider>;
+    return <TaskContext.Provider value={{movieList, movieEdit, deleteMovie, favorite, addMovie, editMovie, updateMovie}}>{children}</TaskContext.Provider>;
 };
 
 export default TaskContext;
